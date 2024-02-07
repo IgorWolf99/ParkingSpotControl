60e12bd97080b6a8f1bc23ff16dc436029dfa7ca
IgorWolf99 <igor.wolf99@gmail.com>
2024-02-07 20:49:24 -0300

    primeiro commit

---
+++ src/test/java/com/api/parkingspotcontrol/ParkingSpotControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parkingspotcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSpotControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
