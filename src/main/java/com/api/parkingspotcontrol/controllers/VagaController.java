f8ca93ee0479b7e1e7c94ea86bb7db95fe255570
IgorWolf99 <igor.wolf99@gmail.com>
2024-02-08 21:54:21 -0300

    Controller - GET

--- src/main/java/com/api/parkingspotcontrol/controllers/VagaController.java
+++ src/main/java/com/api/parkingspotcontrol/controllers/VagaController.java
@@ -3,17 +3,22 @@ package com.api.parkingspotcontrol.controllers;
 import com.api.parkingspotcontrol.entities.Vaga;
 import com.api.parkingspotcontrol.services.ControleDeVagasService;
 import com.api.parkingspotcontrol.services.VagaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/parking-spot-control")
 @CrossOrigin(origins = "*")
 public class VagaController {
+    private static final Logger logger = LoggerFactory.getLogger(VagaService.class);
     @Autowired
     VagaService vagaService;
     @Autowired
@@ -32,6 +37,30 @@ public class VagaController {
         }catch (Exception ex){
             return ResponseEntity.status(HttpStatus.OK).body("ERRO >> DADOS JA EXISTENTES.");
         }
-
     }
+
+    @GetMapping(value = "/vaga/id/{id}")
+    public ResponseEntity<Object> vagaById(@PathVariable UUID id){
+        var vaga = vagaService.findVagaById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(vaga);
+    }
+
+
+    @GetMapping(value = "/vaga/{vagaString}")
+    public ResponseEntity<Object> findByVaga(@PathVariable String vagaString){
+        Optional<Vaga> vaga = vagaService.findByVaga(vagaString);
+        return ResponseEntity.status(HttpStatus.OK).body(vaga);
+    }
+
+    @GetMapping(value = "/vaga/ocupante/{ocupanteId}")
+    public ResponseEntity<Object> vagaPorOcupante(@PathVariable UUID ocupanteId) {
+        Optional<Vaga> vagaOptional = vagaService.findVagaByOcupante(ocupanteId);
+        return  ResponseEntity.status(HttpStatus.OK).body(vagaOptional);
+    }
+
+    @GetMapping(value = {"/vaga/disponiveis"})
+    public ResponseEntity<Object> vagasDisponiveis(){
+        return ResponseEntity.status(HttpStatus.OK).body(vagaService.vagasDisponiveis());
+    }
+
 }
